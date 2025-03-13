e5efa475c834bf67022abba672927fa0510cf4bc
UmairSaifi <umairsaifi2002@gmail.com>
2025-03-13 22:34:01 +0530

    Learn about React Redux Add initial setup for React with Redux and Vite

---
+++ video 120 React Redux/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+function Navbar() {
+    const count = useSelector((state) => state.counter.value)
+    return (
+        <div>
+            This is Navbar {count}
+        </div>
+    )
+}
+
+export default Navbar
