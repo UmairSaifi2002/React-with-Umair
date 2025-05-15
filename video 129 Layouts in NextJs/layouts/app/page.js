5cce06c961be2423e317c7be5d831b98896081f6
UmairSaifi <umairsaifi2002@gmail.com>
2025-05-15 21:24:27 +0530

    some commit

--- video 129 Layouts in NextJs/layouts/app/page.js
+++ video 129 Layouts in NextJs/layouts/app/page.js
@@ -5,8 +5,6 @@ export default function Home() {
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h2 className="text-2xl font-bold underline">Hello world!</h2>
-      <h3 className="text-xl font-bold underline">Hello world!</h3>
-      <h4 className="text-lg font-bold underline">Hello world!</h4>
     </>
   );
 }
